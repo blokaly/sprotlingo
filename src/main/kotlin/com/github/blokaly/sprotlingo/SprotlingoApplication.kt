4ae9b3438bb0dee45726add85158df88a7a7c6e0
Charlie Zhang <blokaly@users.noreply.github.com>
2021-06-06 14:23:19 +0800

    initial commit with spring initializr and mongodb docker

---
+++ src/main/kotlin/com/github/blokaly/sprotlingo/SprotlingoApplication.kt
@@ -0,0 +1,11 @@
+package com.github.blokaly.sprotlingo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SprotlingoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SprotlingoApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/blokaly/sprotlingo/SprotlingoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.blokaly.sprotlingo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SprotlingoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
